accada830974544d3cf053b35f1d9f856866da7e
Petgrave, Ramon <rpetgrave6@gatech.edu>
2025-01-01 02:20:09 -0500

    refactor

--- attr/attr.go
+++ attr/attr.go
@@ -1,10 +1,14 @@
 package attr
 
 import (
-	"fmt"
 	"go-html-compose/render"
 	"html"
 	"io"
+)
+
+var (
+	equalSign = []byte(`=`)
+	quote     = []byte(`"`)
 )
 
 type AttributeStruct struct {
@@ -14,25 +18,22 @@ type AttributeStruct struct {
 }
 
 func (a *AttributeStruct) Render(wr io.Writer) error {
-	if _, err := wr.Write([]byte(fmt.Sprintf(`%s="%s"`, a.Name, html.EscapeString(*a.Value)))); err != nil {
+	var err error
+	if err = render.WriteByteSlices(
+		wr,
+		a.Name, equalSign, quote, []byte(html.EscapeString(*a.Value)), quote,
+	); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (a *AttributeStruct) StructuredRender(wr io.Writer, tabs int) error {
-	var (
-		equalSign = []byte(`=`)
-		quote     = []byte(`"`)
-		err       error
-	)
+	var err error
 	if err = render.WriteTabBytes(wr, tabs); err != nil {
 		return err
 	}
-	if err = render.WriteByteSlices(
-		wr,
-		a.Name, equalSign, quote, []byte(html.EscapeString(*a.Value)), quote,
-	); err != nil {
+	if err = a.Render(wr); err != nil {
 		return err
 	}
 	return nil
