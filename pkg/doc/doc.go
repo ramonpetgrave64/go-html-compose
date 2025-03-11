bd596d36a5bffec9ae96ad5514091fd93c05ebea
Petgrave, Ramon <rpetgrave6@gatech.edu>
2025-03-11 00:13:08 -0400

    renames

--- pkg/doc/doc.go
+++ pkg/doc/doc.go
@@ -7,3 +7,26 @@ import (
 type IContent interface {
 	RenderConent(wr io.Writer) (err error)
 }
+
+// ContContainerStruct contains content without itself being an element.
+// See https://html.spec.whatwg.org/multipage/dom.html#flow-content.
+type ContContainerStruct struct {
+	Children []IContent
+}
+
+// ContContainer creates a ContContainer.
+func ContContainer(children ...IContent) *ContContainerStruct {
+	return &ContContainerStruct{
+		Children: children,
+	}
+}
+
+// RenderContent renders the items in the FlowStruct.
+func (c *ContContainerStruct) RenderConent(wr io.Writer) error {
+	for _, elem := range c.Children {
+		if err := elem.RenderConent(wr); err != nil {
+			return err
+		}
+	}
+	return nil
+}
