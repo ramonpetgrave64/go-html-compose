8e3f0b46be18ed7d0d38ef0e9e63d3dc2417155b
Petgrave, Ramon <rpetgrave6@gatech.edu>
2025-03-18 20:05:28 -0400

    lint

--- pkg/doc/cond_test.go
+++ pkg/doc/cond_test.go
@@ -34,6 +34,8 @@ func Test_If(t *testing.T) {
 		},
 	}
 	for _, tc := range tests {
+		tc := tc
+
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
--- pkg/tag/tag_test.go
+++ pkg/tag/tag_test.go
@@ -34,6 +34,8 @@ func Test_UnitTag(t *testing.T) {
 		},
 	}
 	for _, tc := range tests {
+		tc := tc
+
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
@@ -97,6 +99,8 @@ func Test_ParentTag(t *testing.T) {
 		},
 	}
 	for _, tc := range tests {
+		tc := tc
+
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
